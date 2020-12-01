edce9789457ddcf73114646528b25786b09c9b91
Gautier Ringeisen <gautier.ringeisen@dalim.com>
2020-12-01 08:27:25 +0100

    Locale DE

--- src/gui/locale.jsx
+++ src/gui/locale.jsx
@@ -1,32 +1,52 @@
 cef.locale.define("C", {
 
+	// Application name displayed on the Welcome Page
 	"appname": "DALIM ES Connector",
+	// Splash image displayed on the Welcome Page
 	"splashimage": "./gui/logincard.png",
 
+	// Text in the Connect button
 	"connect": "Connect",
+	// Text replacement in the connect button when connecting
 	"connecting": "Connecting...",
+	// Text in the sign in button
 	"signin": "Sign in",
+	// Tooltip text for the signout icon (settings tab)
 	"signout": "Sign out",
+	// Tooltip text for the change repository icon (settings tab)
 	"change": "Change",
+	// Check box in the login page
 	"rememberme": "Remember me",
+	// Label for the Login field
 	"username.label": "Account name",
+	// Label for the Password field
 	"password.label": "Password",
+	// Label for the server name field
 	"servername.label": "Server URL",
+	// Helper text for the server name field
 	"servername.helpertext": "Enter a repository URL or choose one in the history below",
+	// Placeholder text for the server name field
 	"servername.placeholder": "ex: www.your-dalim-es.com",
+
+	// File size units
 	"datasizes": ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb'],
 	
+	// Used zhen selecting multiple items in a list. Will display "14 selected"
 	"selected": "selected",
+
+	// Asset and links statuses
 	"local": "Local",
 	"missing": "Missing",
 	"unknown": "Unknown",
 	"localcache": "Local cache",
 	"fileconflict": "File changed locally and remotely",
 
+	// Main interface Tab names
 	"Browser": "Browser",
 	"Document": "Document",
 	"Settings" : "Settings",
 
+	// Asset and links property names (mainly displayed in the expandable property panel)
 	"AssetID" : "Asset ID",
 	"AssetName" : "Asset name",
 	"AssetSize" : "Asset size",
@@ -41,21 +61,32 @@ cef.locale.define("C", {
 	"FilePath": "File path",
 	"FileSize": "File size",
 	"LastModified": "Last modified",
-	"DocumentLinks": "Document links",
-	"Preferences": "Preferences",
-	"Connection": "Connection",
 	"Version": "Version",
+	
+	// Document link list title
+	"DocumentLinks": "Document links",
+
+	// Heading text for user preferences displayed in the settings tab
+	"Preferences": "Preferences",
+	// Heading text for the connection information displayed in the settings tab
+	"Connection": "Connection",
+	// Heading text for the cache information displayed in the settings tab
 	"Cache": "Cache",
+
+	// Some preferences and settings name
 	"AutoSave": "Auto save on upload",
 	"UseHighResolution": "Use high resolution images",
 	"PDFExportPreset": "PDF Export Preset",
+	"Account": "Account",
 	"Folder": "Folder",
 	"Size": "Size",
-	"Account": "Account",
+	
+	// OK action
 	"Ok": "Ok",
+	// Cancel action
 	"Cancel": "Cancel",
 
-	// Buttons and actions
+	// Tootips on action buttons
 	"uploadDocument": "Upload document",
 	"checkAssetOut": "Check asset out",
 	"checkDocumentIn": "Check document in",
@@ -77,7 +108,7 @@ cef.locale.define("C", {
 	"unlockAsset": "Unlock",
 	"showAsset": "Show in repository",
 
-	// States
+	// Asset and files states (displayed beside the asset while performing the action)
 	"downloading": "Downloading...",
 	"uploading": "Uploading...",
 	"exporting": "Exporting PDF...",
@@ -115,7 +146,118 @@ cef.locale.define("en", {
 });
 
 cef.locale.define("de", {
-	
+
+	"appname": "DALIM ES Steckverbinder",
+    "splashimage": "./gui/logincard.png",
+    "connect": "Verbinden",
+    "connecting": "Verbindung...",
+    "signin": "Anmeldung",
+    "signout": "Abmeldung",
+    "change": "Ändern",
+    "rememberme": "Login speichern",
+    "username.label": "Kontoname",
+    "password.label": "Kennwort",
+    "servername.label": "Server URL",
+    "servername.helpertext": "Geben Sie eine Repository-URL ein oder wählen Sie eine in der nachstehenden Historie",
+    "servername.placeholder": "Beispiel: www.your-dalim-es.com",
+    "datasizes": ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb'],
+    
+    // Used zhen selecting multiple items in a list. Will display "14 selected"
+    "selected": "ausgewählt",
+
+    // Asset and links statuses
+    "local": "Lokal",
+    "missing": "Fehlend",
+    "unknown": "Unbekannt",
+    "localcache": "Lokaler Cache",
+    "fileconflict": "Datei lokal und entfernt geändert",
+
+    // Main interface Tab names
+    "Browser": "Browser",
+    "Document": "Dokument",
+    "Settings" : "Einstellungen",
+
+    // Asset and links property names (mainly displayed in the expandable property panel)
+    "AssetID" : "Asset ID",
+    "AssetName" : "Assetname",
+    "AssetSize" : "Assetgröße",
+    "AssetType" : "Assettyp",
+    "AssetPath" : "Assetpfad",
+    "AssetVersion" : "Assetversion",
+    "Repository" : "Repository",
+    "Location": "Lage",
+    "LockedBy" : "Gesperrt durch",
+    "Rendition": "Wiedergabe",
+    "FileName": "Dateiname",
+    "FilePath": "Dateipfad",
+    "FileSize": "Dateigröße",
+    "LastModified": "Zuletzt geändert",
+    "Version": "Version",
+    
+    // Document link list title
+    "DocumentLinks": "Dokumentverknüpfungen",
+
+    // Heading text for user preferences displayed in the settings tab
+    "Preferences": "Präferenzen",
+    "Connection": "Verbindung",
+    "Cache": "Cache",
+
+    // Some preferences and settings name
+    "AutoSave": "Automatisches Speichern beim Hochladen",
+    "UseHighResolution": "Hochauflösende Bilder verwenden",
+    "PDFExportPreset": "Voreinstellung für PDF-Export",
+    "Account": "Konto",
+    "Folder": "Ordner",
+    "Size": "Größe",
+
+	// Confirm buttons
+    "Ok": "Ok",
+    "Cancel": "Abbrechen",
+
+    // Tootips on action buttons
+    "uploadDocument": "Dokument hochladen",
+    "checkAssetOut": "Asset auschecken",
+    "checkDocumentIn": "Dokument einchecken",
+    "exportDocumentAsPDF": "PDF exportieren",
+    "uploadAllLocalLinks": "Alle lokalen Verbindungen hochladen",
+    "downloadAllMissingLinks": "Alle fehlenden Verbindungen herunterladen",
+    "placeAsset": "Asset platzieren",
+    "unlinkAsset": "Asset lösen",
+    "linkNonHTTPAssets": "Nicht-HTTP Assets verknüpfen",
+    "downloadLink": "Asset herunterladen",
+    "uploadLink": "Verknüpfte Datei hochladen",
+    "checkLinkIn": "Verknüpfung einchecken",
+    "toggleRendition": "Wiedergabe umschalten",
+    "setLinkHighres": "Verknüpfung Highres setzen",
+    "setLinkLowres": "Verknüpfung Lowres setzen",
+    "lockDocument": "Sperren",
+    "unlockDocument": "Entsperren",
+    "lockAsset": "Sperren",
+    "unlockAsset": "Entsperren",
+    "showAsset": "Im Repository anzeigen",
+
+    // Asset and files states (displayed beside the asset while performing the action)
+    "downloading": "Herunterladung...",
+    "uploading": "Hochladung...",
+    "exporting": "PDF Export...",
+    "checkingout": "Auscheckung...",
+    "checkingin": "Eincheckung...",
+    "relinking": "Neuverknüpfung...",
+
+    // Alert messages
+    "document_has_local_links": "Einige Assets in diesem Dokument wurden nicht auf ein Repository hochgeladen.\nEs wird empfohlen, alle Assets vor dem Dokument hochzuladen..\n\nWollen Sie das Dokument immer noch hochladen ?", 
+    "document_uploaded": "Dokument '{0}' erfolgreich hochgeladen",
+    "document_checked_in": "Dokument '{0}' erfolgreich eingecheckt",
+    "pdf_exported": "PDF '{0}' erfolgreich exportiert",
+    "{0}_asset_linked": "{0} Asset verknüpft",
+    "{0}_assets_linked": "{0} Assets verknüpft",
+
+    // Renditions
+    "dalim:highresolution": "Hochauflösung",
+    "dalim:highres": "Hochauflösung",
+    "dalim:lowres": "Niedrige Auflösung",
+    "dalim:preview": "Voransicht",
+    "dalim:thumbnail": "Miniaturbild",
 });
 
 cef.locale.define("fr", {
@@ -217,8 +359,4 @@ cef.locale.define("fr", {
 
 	// File types
 	"video/quicktime": "Vidéo QuickTime"
-});
-
-cef.locale.define("de", {
-	
 });
